7b637c524bde1b7a95ac9bda156e3eb47d9180e5
TyHagan808 <tyhagan@calpoly.edu>
2026-02-26 01:53:54 -0800

    Added user location markers from example code and tweaked for zoom

--- packages/react-frontend/src/components/MapPage.jsx
+++ packages/react-frontend/src/components/MapPage.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 import L from "leaflet";
 import markerIconUrl from "leaflet/dist/images/marker-icon.png";
 import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
 
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Tooltip} from 'react-leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -12,6 +14,25 @@ L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIconRetinaUrl,
   shadowUrl: markerShadowUrl,
 });
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, 21)
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Tooltip>You are here, you lost ah mf</Tooltip>
+    </Marker>
+  )
+}
 
 function MapPage() {
   return (
@@ -68,17 +89,19 @@ function MapPage() {
         </div>
 
         <h2>WYA??</h2>
-        <div style={{ height: "calc(100vh - 140px)", width: "50%" }}>
+        <div style={{ height: "calc(100vh - 140px)", width: "100%" }}>
             <MapContainer 
                 center={[20.74085079117805, -156.2268428410732]} 
                 zoom={12} 
                 scrollWheelZoom={true}
                 style= {{height: "100%", width: "100%"}}
+                maxZoom={21}
             >
                 {/* Map Overlay */}
                 <TileLayer
                     attribution='Tiles &copy; Esri'
                     url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                    maxZoom={21}
                 />
 
                 {/* Boundaries and Labels Oberlay */}
@@ -87,12 +110,9 @@ function MapPage() {
                     url="https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}"
                 />
 
-                {/* Marker at paliku cabin rn */}
-                <Marker position={[20.717873679279453, -156.14192279787463]}>
-                    <Popup>
-                    This would be where a comment is <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {/* Marker Of Current Postition */}
+                <LocationMarker/>
+
             </MapContainer>
         </div>
     </div>
