ee4c07dad85d074c9475306871863c725856c5df
BobbyWoodard <bobbywoodard11@gmail.com>
2026-05-05 16:07:44 -0700

    Added eslint.config in root

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        files: ["**/*.js"],
+        ignores: ["node_modules/**"],
+        languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "commonjs",
+        },
+        rules: {
+        "no-unused-vars": "warn",
+        "no-undef": "error",
+        },
+    },
+];
